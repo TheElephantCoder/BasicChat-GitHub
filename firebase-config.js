8ffd4353fc5839a26c3a06063cea6423c481d89d
TheElephantCoder <157982101+TheElephantCoder@users.noreply.github.com>
2024-12-10 20:52:42 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBactpZhcWroA7JZCTgnB0Q6zK4JUni3aU",
+  authDomain: "basicchat-github.firebaseapp.com",
+  databaseURL: "https://basicchat-github-default-rtdb.firebaseio.com",
+  projectId: "basicchat-github",
+  storageBucket: "basicchat-github.firebasestorage.app",
+  messagingSenderId: "342258367950",
+  appId: "1:342258367950:web:a2cea31ffe1fe9ee133f9f",
+  measurementId: "G-ZR5TY0KFYV"
+};
